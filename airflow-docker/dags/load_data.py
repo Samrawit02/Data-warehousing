1fa7e9b2f657c8bb21d61d1feb8fd6b48172126a
Samrawit02 <hairaer02@gmail.com>
2022-07-25 18:43:01 +0300

    creating and loading data dag

---
+++ airflow-docker/dags/load_data.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.mysql_operator import MySqlOperator
+
+default_arg = {'owner': 'airflow', 'start_date': '2022-07-23'}
+
+dag = DAG('load-data',
+          default_args=default_arg,
+          schedule_interval='@once',
+          template_searchpath=['/usr/local/airflow/include/'])
+
+create_table = MySqlOperator(dag=dag,
+                           mysql_conn_id='mysql-connect', 
+                           task_id='create_table',
+                            sql='create_table.sql')
+
+
+
+load_data = MySqlOperator(dag=dag,
+                           mysql_conn_id='mysql-connect', 
+                           task_id='load_data',
+                            sql='load_data.sql')
+
+
+create_table >> load_data 
